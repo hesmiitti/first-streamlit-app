55f093a5c9e7919743a5c37115b3352b558dbafd
hesmiitti <157386571+hesmiitti@users.noreply.github.com>
2024-02-07 09:42:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 streamlit.subheader('Vähä alaotsikkoa')
 
 streamlit.text('ja tekstiä aoaoa äöäö ÄÖÄÖÄÄÖ')
