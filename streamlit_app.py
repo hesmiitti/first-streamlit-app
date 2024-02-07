581d4e792d12970daa9568b0efe114fc76e8d609
hesmiitti <157386571+hesmiitti@users.noreply.github.com>
2024-02-07 09:40:00 +0200

    Update streamlit_app.py
    lisätty alaotsikkoa ja tekstiä

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 
 streamlit.title('Streamlitin otsikko tässä')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.subheader('Vähä alaotsikkoa')
 
