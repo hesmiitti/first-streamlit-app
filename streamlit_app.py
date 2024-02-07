8f12ffa09a3a6c292a30ef97c0fed9b5cf7a095b
hesmiitti <157386571+hesmiitti@users.noreply.github.com>
2024-02-07 09:46:39 +0200

    Update streamlit_app.py lisätty pandas ja luettu csv nettisivult

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,8 @@ streamlit.subheader('Vähä alaotsikkoa')
 
 streamlit.text('ja tekstiä aoaoa äöäö ÄÖÄÖÄÄÖ')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
