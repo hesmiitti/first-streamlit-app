29a37fe5cfff9b307a76837d1ec420351e827613
hesmiitti <157386571+hesmiitti@users.noreply.github.com>
2024-02-07 09:41:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title('Streamlitin otsikko tässä')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
 streamlit.subheader('Vähä alaotsikkoa')
 
 streamlit.text('ja tekstiä aoaoa äöäö ÄÖÄÖÄÄÖ')
