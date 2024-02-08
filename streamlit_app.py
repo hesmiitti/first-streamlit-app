2e7b3cf843bebd1a90ea9b7d666cdcf28fefedb2
hesmiitti <157386571+hesmiitti@users.noreply.github.com>
2024-02-08 15:30:30 +0200

    Uuuuupdate streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,13 +47,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("Fruit_Load_List contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.header("Lisää hedelma")
-
+#streamlit.header("Lisää hedelma")
 
 #add_my_fruit = streamlit.text_input('Anna uusi lisättävä hedelmä:','')
-#my_cur.execute("insert into FRUIT_LOAD_LIST values ('addingia')");
+#my_cur.execute("insert into FRUIT_LOAD_LIST values ('addingia')")
 #streamlit.write('Kiitos, että lisäsit hedelmän: ', add_my_fruit)
   
-
-
-
